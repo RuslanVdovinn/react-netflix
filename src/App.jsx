1e05c7eb79447dbffb014b372494f58266451d50
Ruslan Vdovin <vdovin@mgalaxy.pro>
2025-11-13 12:53:41 +0700

    movie_card

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MovieCard } from './MovieCard'
+import { MOVIES } from './movies.data'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="min-h-screen w-full bg-black text-white px-6 py-5">
+        <header className="mb-10 flex items-center">
+          <img 
+              src="/netflix.png"
+              alt="Netflix" 
+              className='h-8 w-auto'
+          />
+        </header>
+        <main className='flex gap-6'>
+          {
+            MOVIES.map(movie => {
+            return (
+                <MovieCard 
+                  key={movie.name}
+                  image={movie.image} 
+                  rating={movie.rating} 
+                />
+              )
+            })
+          }
+        </main>
+    </div>
   )
 }
 
