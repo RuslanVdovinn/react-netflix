12af7dba8e9d66a960fedc88077ca2938b4c4538
Ruslan Vdovin <vdovin@mgalaxy.pro>
2025-11-17 13:33:17 +0700

    finish

--- src/MainRoutes.jsx
+++ src/MainRoutes.jsx
@@ -1,19 +1,22 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import App from "./App";
-import { MovieDetails } from "./MovieDetails";
+import App from "./screens/home/App";
+import { MovieDetails } from "./screens/movie/MovieDetails";
+import { Layout } from "./components/Layout";
 
 export function MainRoutes() {
     return (
         <Router>
             <Routes>
-                <Route
-                    path="/"
-                    element={<App />}
-                />
-                <Route
-                    path="/movie/:id"
-                    element={<MovieDetails />}
-                />
+                <Route element={<Layout />}>
+                    <Route
+                        path="/"
+                        element={<App />}
+                    />
+                    <Route
+                        path="/movie/:id"
+                        element={<MovieDetails />}
+                    />
+                </Route>
             </Routes>
         </Router>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './screens/home/App.jsx'
 import { ThemeProvider } from './ThemeProvider.jsx'
 import { MainRoutes } from './MainRoutes.jsx'
 
